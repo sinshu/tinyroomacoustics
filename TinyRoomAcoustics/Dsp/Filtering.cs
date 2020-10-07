2ed7b7184b710e32a435a0b24f9368f3ed8f5412
sinshu <sinshu.d@gmail.com>
2020-10-07 20:34:42 +0900

    Add FFT-based fast convolution.

---
+++ TinyRoomAcoustics/Dsp/Filtering.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+
+namespace TinyRoomAcoustics.Dsp
+{
+    public static class Filtering
+    {
+        public static double[] Convolve(this double[] source, double[] fir)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (fir == null)
+            {
+                throw new ArgumentNullException(nameof(fir));
+            }
+
+            if (fir.Length == 0)
+            {
+                throw new ArgumentException(nameof(fir), "The length of the FIR must be non-zero.");
+            }
+
+            var dftLength = GetMinPot(2 * fir.Length);
+            var frameLength = dftLength / 2;
+
+            var firDft = new Complex[dftLength];
+            for (var t = 0; t < fir.Length; t++)
+            {
+                firDft[t] = fir[t];
+            }
+            Fourier.Forward(firDft, FourierOptions.AsymmetricScaling);
+
+            var destination = new double[source.Length];
+
+            var frameDft = new Complex[dftLength];
+
+            for (var position = 0; position < source.Length; position += frameLength)
+            {
+                {
+                    var destinationEnd = position + frameLength;
+                    var frameEnd = frameLength;
+
+                    var exceedance = destinationEnd - source.Length;
+                    if (exceedance > 0)
+                    {
+                        destinationEnd -= exceedance;
+                        frameEnd -= exceedance;
+                    }
+
+                    for (var t = 0; t < frameEnd; t++)
+                    {
+                        frameDft[t] = source[position + t];
+                    }
+                    for (var t = frameEnd; t < frameDft.Length; t++)
+                    {
+                        frameDft[t] = 0;
+                    }
+                    Fourier.Forward(frameDft, FourierOptions.AsymmetricScaling);
+                }
+
+                frameDft[0] *= firDft[0];
+                for (var w = 1; w < frameLength; w++)
+                {
+                    frameDft[w] *= firDft[w];
+                    frameDft[frameDft.Length - w] = frameDft[w].Conjugate();
+                }
+                frameDft[frameLength] *= firDft[frameLength];
+                Fourier.Inverse(frameDft, FourierOptions.AsymmetricScaling);
+
+                {
+                    var destinationEnd = position + dftLength;
+                    var frameEnd = dftLength;
+
+                    var exceedance = destinationEnd - source.Length;
+                    if (exceedance > 0)
+                    {
+                        destinationEnd -= exceedance;
+                        frameEnd -= exceedance;
+                    }
+
+                    for (var t = 0; t < frameEnd; t++)
+                    {
+                        destination[position + t] += frameDft[t].Real;
+                    }
+                }
+            }
+
+            return destination;
+        }
+
+        private static int GetMinPot(int value)
+        {
+            var pot = 1;
+            while (pot < value)
+            {
+                pot *= 2;
+            }
+            return pot;
+        }
+    }
+}
---
+++ TinyRoomAcousticsTest/DspTest/FilteringTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+using TinyRoomAcoustics.Dsp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TinyRoomAcousticsTest
+{
+    [TestClass]
+    public class FilteringTest
+    {
+        [DataTestMethod]
+        [DataRow(100, 8)]
+        [DataRow(123, 16)]
+        [DataRow(25, 32)]
+        [DataRow(100, 7)]
+        [DataRow(123, 12)]
+        [DataRow(25, 57)]
+        [DataRow(10, 1)]
+        public void Convolve(int sourceLength, int firLength)
+        {
+            var random = new Random(57);
+            var source = Enumerable.Range(0, sourceLength).Select(t => random.NextDouble()).ToArray();
+            var fir = Enumerable.Range(0, firLength).Select(t => random.NextDouble()).ToArray();
+
+            var expected = Convolve_Naive(source, fir);
+            var actual = Filtering.Convolve(source, fir);
+            for (var t = 0; t < actual.Length; t++)
+            {
+                Assert.AreEqual(expected[t], actual[t], 1.0E-6);
+            }
+        }
+
+        private double[] Convolve_Naive(double[] source, double[] fir)
+        {
+            var destination = new double[source.Length];
+            for (var dt = 0; dt < source.Length; dt++)
+            {
+                var sum = 0.0;
+                var du = dt;
+                for (var ft = 0; ft < fir.Length; ft++)
+                {
+                    if (du >= 0)
+                    {
+                        sum += fir[ft] * source[du];
+                        du--;
+                    }
+                }
+                destination[dt] = sum;
+            }
+            return destination;
+        }
+    }
+}
