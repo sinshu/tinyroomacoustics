8a57c6ca3517d6aa51caad906667a1f7b4a1c14c
sinshu <sinshu.d@gmail.com>
2020-10-07 13:56:33 +0900

    Add window functions.

---
+++ TinyRoomAcoustics/Dsp/WindowFunctions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace TinyRoomAcoustics.Dsp
+{
+    public static class WindowFunctions
+    {
+        public static double[] Hann(int length)
+        {
+            var window = new double[length];
+            for (var t = 0; t < length; t++)
+            {
+                var x = 2 * Math.PI * t / length;
+                window[t] = (1 - Math.Cos(x)) / 2;
+            }
+            return window;
+        }
+
+        public static double[] SqrtHann(int length)
+        {
+            var window = new double[length];
+            for (var t = 0; t < length; t++)
+            {
+                var x = 2 * Math.PI * t / length;
+                window[t] = Math.Sqrt((1 - Math.Cos(x)) / 2);
+            }
+            return window;
+        }
+    }
+}
---
+++ TinyRoomAcousticsTest/Dsp/WindowFunctionsTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using TinyRoomAcoustics.Dsp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TinyRoomAcousticsTest
+{
+    [TestClass]
+    public class WindowFunctionsTest
+    {
+        [TestMethod]
+        public void Hann()
+        {
+            var hann = WindowFunctions.Hann(256);
+
+            Assert.AreEqual(256, hann.Length);
+
+            Assert.AreEqual(0.0, hann.First(), 1.0E-6);
+            Assert.AreEqual(0.0, hann.Last(), 0.001);
+
+            Assert.AreEqual(1.0, hann[128], 1.0E-6);
+
+            Assert.AreEqual(0.5, hann[64], 1.0E-6);
+            Assert.AreEqual(0.5, hann[192], 1.0E-6);
+        }
+
+        [TestMethod]
+        public void SqrtHann()
+        {
+            var hann = WindowFunctions.Hann(256);
+
+            var sqrtHann = WindowFunctions.SqrtHann(256);
+
+            for (var t = 0; t < 256; t++)
+            {
+                Assert.AreEqual(Math.Sqrt(hann[t]), sqrtHann[t], 1.0E-6);
+            }
+        }
+    }
+}
