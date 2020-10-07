6d87156e283ab5e17b6f534485c9d6595c41a0ab
sinshu <sinshu.d@gmail.com>
2020-10-07 21:07:21 +0900

    Improve convolution test.

--- TinyRoomAcousticsTest/DspTest/FilteringTest_Convolve.cs
+++ TinyRoomAcousticsTest/DspTest/FilteringTest_Convolve.cs
@@ -10,7 +10,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TinyRoomAcousticsTest
 {
     [TestClass]
-    public class FilteringTest
+    public class FilteringTest_Convolve
     {
         [DataTestMethod]
         [DataRow(100, 8)]
@@ -20,7 +20,31 @@ namespace TinyRoomAcousticsTest
         [DataRow(123, 12)]
         [DataRow(25, 57)]
         [DataRow(10, 1)]
-        public void Convolve(int sourceLength, int firLength)
+        public void Convolve_CheckWithDelayFilter(int sourceLength, int firLength)
+        {
+            var random = new Random(57);
+            var source = Enumerable.Range(0, sourceLength).Select(t => random.NextDouble()).ToArray();
+            
+            var delayFilter = new double[firLength];
+            delayFilter[delayFilter.Length - 1] = 1.0;
+
+            var expected = Enumerable.Repeat(0.0, firLength - 1).Concat(source).Take(sourceLength).ToArray();
+            var actual = Filtering.Convolve(source, delayFilter);
+            for (var t = 0; t < actual.Length; t++)
+            {
+                Assert.AreEqual(expected[t], actual[t], 1.0E-6);
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow(100, 8)]
+        [DataRow(123, 16)]
+        [DataRow(25, 32)]
+        [DataRow(100, 7)]
+        [DataRow(123, 12)]
+        [DataRow(25, 57)]
+        [DataRow(10, 1)]
+        public void Convolve_CheckWithNaiveImplementation(int sourceLength, int firLength)
         {
             var random = new Random(57);
             var source = Enumerable.Range(0, sourceLength).Select(t => random.NextDouble()).ToArray();
