70bcb649e0a7da187c86414897782ea2d59ac35f
sinshu <sinshu.d@gmail.com>
2020-10-10 17:15:56 +0900

    Add tests for steering vector generation.

--- TinyRoomAcousticsTest/BeamformingTest/SteeringVectorTest_FromFarFieldGeometry.cs
+++ TinyRoomAcousticsTest/BeamformingTest/SteeringVectorTest_FromFarFieldGeometry.cs
@@ -85,9 +85,52 @@ namespace TinyRoomAcousticsTest
 
             var delayFilters = new Complex[][]
             {
-                Filtering.CreateFrequencyDomainDelayFilter(dftLength, 0 * Math.Sqrt(2) / 2),
-                Filtering.CreateFrequencyDomainDelayFilter(dftLength, 3 * Math.Sqrt(2) / 2),
-                Filtering.CreateFrequencyDomainDelayFilter(dftLength, 6 * Math.Sqrt(2) / 2)
+                Filtering.CreateFrequencyDomainDelayFilter(dftLength, 0 / Math.Sqrt(2)),
+                Filtering.CreateFrequencyDomainDelayFilter(dftLength, 3 / Math.Sqrt(2)),
+                Filtering.CreateFrequencyDomainDelayFilter(dftLength, 6 / Math.Sqrt(2))
+            };
+
+            for (var w = 0; w < dftLength / 2 + 1; w++)
+            {
+                for (var ch = 0; ch < 3; ch++)
+                {
+                    var actual = sv[w][ch];
+                    var expected = delayFilters[ch][w];
+                    Assert.AreEqual(expected.Real, actual.Real, 1.0E-6);
+                    Assert.AreEqual(expected.Imaginary, actual.Imaginary, 1.0E-6);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void CheckWithDelayFilter_Case3()
+        {
+            var delaySampleCount = 3;
+
+            var sampleRate = 16000;
+            var dftLength = 1024;
+
+            var time = (double)delaySampleCount / sampleRate;
+            var distance = AcousticConstants.SoundSpeed * time;
+
+            var soundSource = new SoundSource(1.0, 1.0, 1.0);
+
+            var microphones = new Microphone[]
+            {
+                new Microphone(1.0 + 1 * distance, 1.0, 1.0),
+                new Microphone(1.0 + 2 * distance, 1.0, 1.0),
+                new Microphone(1.0 + 3 * distance, 1.0, 1.0)
+            };
+
+            var sv = SteeringVector.FromFarFieldGeometry(microphones, 0.0, Math.PI / 4, sampleRate, dftLength);
+
+            Assert.AreEqual(dftLength / 2 + 1, sv.Length);
+
+            var delayFilters = new Complex[][]
+            {
+                Filtering.CreateFrequencyDomainDelayFilter(dftLength, 0 / Math.Sqrt(2)),
+                Filtering.CreateFrequencyDomainDelayFilter(dftLength, 3 / Math.Sqrt(2)),
+                Filtering.CreateFrequencyDomainDelayFilter(dftLength, 6 / Math.Sqrt(2))
             };
 
             for (var w = 0; w < dftLength / 2 + 1; w++)
