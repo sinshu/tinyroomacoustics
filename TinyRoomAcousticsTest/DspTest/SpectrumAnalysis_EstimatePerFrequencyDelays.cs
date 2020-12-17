6d7798acb5867cde47edd6f9902ba2e2f3e9e523
sinshu <sinshu.d@gmail.com>
2020-12-17 19:24:54 +0900

    Add more test for delay estimation.

--- TinyRoomAcousticsTest/DspTest/SpectrumAnalysis_EstimatePerFrequencyDelays.cs
+++ TinyRoomAcousticsTest/DspTest/SpectrumAnalysis_EstimatePerFrequencyDelays.cs
@@ -20,9 +20,9 @@ namespace TinyRoomAcousticsTest
         [DataRow(512, 12, 10)]
         [DataRow(1024, 16, 32)]
         [DataRow(1024, 32, 16)]
-        public void CheckWithIntegerDelay(int frameLength, int peakPosition1, int peakPosition2)
+        public void CheckWithIntegerDelay_Impulse(int frameLength, int peakPosition1, int peakPosition2)
         {
-            var expected = peakPosition2 - peakPosition1;
+            var expectedDelay = peakPosition2 - peakPosition1;
 
             var x = new Complex[frameLength];
             var y = new Complex[frameLength];
@@ -39,9 +39,51 @@ namespace TinyRoomAcousticsTest
             {
                 var waveLength = (double)frameLength / w;
 
-                if (Math.Abs(expected) + 1.0E-6 < waveLength / 2)
+                if (Math.Abs(expectedDelay) + 1.0E-6 < waveLength / 2)
                 {
-                    Assert.AreEqual(expected, delays[w], 1.0E-6);
+                    Assert.AreEqual(expectedDelay, delays[w], 1.0E-6);
+                }
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow(64, 1)]
+        [DataRow(64, -1)]
+        [DataRow(512, 2)]
+        [DataRow(512, -2)]
+        [DataRow(1024, 16)]
+        [DataRow(1024, -16)]
+        public void CheckWithIntegerDelay_WhiteNoise(int frameLength, int expectedDelay)
+        {
+            var random = new Random(57);
+
+            var x = new Complex[frameLength];
+            var y = new Complex[frameLength];
+
+            for (var t = 0; t < frameLength; t++)
+            {
+                var u = (t + expectedDelay) % frameLength;
+                if (u < 0)
+                {
+                    u += frameLength;
+                }
+
+                x[t] = 2 * random.NextDouble() - 1;
+                y[u] = x[t];
+            }
+
+            Fourier.Forward(x, FourierOptions.AsymmetricScaling);
+            Fourier.Forward(y, FourierOptions.AsymmetricScaling);
+
+            var delays = SpectrumAnalysis.EstimatePerFrequencyDelays(x, y);
+
+            for (var w = 1; w < delays.Length; w++)
+            {
+                var waveLength = (double)frameLength / w;
+
+                if (Math.Abs(expectedDelay) + 1.0E-6 < waveLength / 2)
+                {
+                    Assert.AreEqual(expectedDelay, delays[w], 1.0E-6);
                 }
             }
         }
