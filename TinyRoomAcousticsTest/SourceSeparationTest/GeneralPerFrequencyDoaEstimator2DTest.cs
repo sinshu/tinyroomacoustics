8a99ddace9ee8e108c7f223c3f4d28dc13f7ecb5
sinshu <sinshu.d@gmail.com>
2020-12-25 21:38:57 +0900

    Improve DOA estimation test.

--- TinyRoomAcousticsTest/SourceSeparationTest/GeneralPerFrequencyDoaEstimator2DTest.cs
+++ TinyRoomAcousticsTest/SourceSeparationTest/GeneralPerFrequencyDoaEstimator2DTest.cs
@@ -30,42 +30,7 @@ namespace TinyRoomAcousticsTest
 
             var maxSpace = Math.Sqrt(2) * 0.03;
 
-            var estimator = new GeneralPerFrequencyDoaEstimator2D(microphones, sampleRate, frameLength);
-
-            for (var deg = 1; deg < 360; deg++)
-            {
-                var expectedDoa = Math.PI * deg / 180 - Math.PI;
-
-                MathNet.Numerics.LinearAlgebra.Vector<double> dv = DenseVector.OfArray(new double[]
-                {
-                    Math.Cos(expectedDoa),
-                    Math.Sin(expectedDoa),
-                    0
-                });
-
-                var dfts = new Complex[frameLength][];
-                for (var ch = 0; ch < microphones.Length; ch++)
-                {
-                    var distance = microphones[ch].Position * dv;
-                    var delaySampleCount = -distance / AcousticConstants.SoundSpeed * sampleRate;
-                    var delayFilter = Filtering.CreateFrequencyDomainDelayFilter(frameLength, delaySampleCount);
-                    var dft = new Complex[frameLength];
-                    Array.Copy(delayFilter, dft, delayFilter.Length);
-                    dfts[ch] = dft;
-                }
-
-                var actualDoa = estimator.Estimate(dfts);
-
-                for (var w = 1; w < frameLength / 2 + 1; w++)
-                {
-                    var waveLength = (double)frameLength / w / sampleRate * AcousticConstants.SoundSpeed;
-
-                    if (maxSpace + 1.0E-6 < waveLength / 2)
-                    {
-                        Assert.AreEqual(expectedDoa, actualDoa[w], 1.0E-6);
-                    }
-                }
-            }
+            EstimateCore(microphones, sampleRate, frameLength, maxSpace);
         }
 
         [TestMethod]
@@ -83,6 +48,30 @@ namespace TinyRoomAcousticsTest
 
             var maxSpace = 0.03;
 
+            EstimateCore(microphones, sampleRate, frameLength, maxSpace);
+        }
+
+        [TestMethod]
+        public void Estimate3()
+        {
+            var sampleRate = 16000;
+            var frameLength = 1024;
+
+            var microphones = new Microphone[]
+            {
+                new Microphone(0.00, 0.00, 1.0),
+                new Microphone(0.02, 0.00, 1.0),
+                new Microphone(0.00, 0.02, 1.0),
+                new Microphone(0.02, 0.02, 1.0)
+            };
+
+            var maxSpace = Math.Sqrt(2) * 0.02;
+
+            EstimateCore(microphones, sampleRate, frameLength, maxSpace);
+        }
+
+        private void EstimateCore(Microphone[] microphones, int sampleRate, int frameLength, double maxSpace)
+        {
             var estimator = new GeneralPerFrequencyDoaEstimator2D(microphones, sampleRate, frameLength);
 
             for (var deg = 1; deg < 360; deg++)
