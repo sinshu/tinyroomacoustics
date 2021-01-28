cf4c2a19780b4bdf9436aaf3027f79d3e7433533
sinshu <sinshu.d@gmail.com>
2021-01-28 16:16:55 +0900

    Add TSP generator.

---
+++ TinyRoomAcoustics/Dsp/SignalGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+
+namespace TinyRoomAcoustics.Dsp
+{
+    public static class SignalGenerator
+    {
+        public static double[] Tsp(int length)
+        {
+            double n = length;
+            double j = n / 2;
+
+            var fft = new Complex[length];
+
+            for (var k = 0; k < length / 2 + 1; k++)
+            {
+                fft[k] = Complex.Exp(-Complex.ImaginaryOne * 2 * Math.PI * j * ((k / n) * (k / n)));
+            }
+
+            for (var k = 1; k < length / 2; k++)
+            {
+                fft[length - k] = fft[k].Conjugate();
+            }
+
+            Fourier.Inverse(fft, FourierOptions.AsymmetricScaling);
+
+            var result = fft.Select(x => x.Real).ToArray();
+            var max = result.Select(x => Math.Abs(x)).Max();
+            for (var i = 0; i < result.Length; i++)
+            {
+                result[i] = 0.9 * result[i] / max;
+            }
+
+            var shift = (int)((n - j) / 2);
+            var result2 = new double[result.Length];
+            for (var i = 0; i < result.Length; i++)
+            {
+                var i2 = (i + shift) % result.Length;
+                result2[i2] = result[i];
+            }
+
+            return result2;
+        }
+    }
+}
--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -7,6 +7,7 @@ using MathNet.Numerics;
 using MathNet.Numerics.IntegralTransforms;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
+using NAudio.Wave;
 using TinyRoomAcoustics;
 using TinyRoomAcoustics.Beamforming;
 using TinyRoomAcoustics.Dsp;
@@ -19,6 +20,47 @@ namespace Workspace
     {
         static void Main(string[] args)
         {
+            var test = SignalGenerator.Tsp(65536);
+            var format = new WaveFormat(48000, 1);
+            using (var writer = new WaveFileWriter("test.wav", format))
+            {
+                foreach (var value in test)
+                {
+                    writer.WriteSample((float)value);
+                }
+            }
+
+            var test2 = test.Concat(new double[test.Length]).ToArray();
+
+            var inv = test.Reverse().ToArray();
+            var imp = Filtering.Convolve(test2, inv);
+            var max = imp.Select(x => Math.Abs(x)).Max();
+            imp = imp.Select(x => 0.95 * x / max).ToArray();
+            using (var writer = new WaveFileWriter("test2.wav", format))
+            {
+                foreach (var value in imp)
+                {
+                    writer.WriteSample((float)value);
+                }
+            }
+
+            var sum = imp.Select(x => Math.Abs(x)).Sum();
+            Console.WriteLine(sum);
+
+            var pos = 0;
+            for (var i = 0; i < imp.Length; i++)
+            {
+                if (imp[i] > 0.9)
+                {
+                    pos = i;
+                    Console.WriteLine("PEAK!");
+                }
+            }
+
+            for (var i = pos - 10; i <= pos + 10; i++)
+            {
+                Console.WriteLine(imp[i]);
+            }
         }
     }
 }
